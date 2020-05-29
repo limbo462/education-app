b07393d62d525c87ef45472168b5ccd535447e30
viditchopra1500 <vidit1500@gmail.com>
2020-05-29 20:34:16 +0530

    Updates

---
+++ precache-manifest.17fb39a2f8dc48c4d51cbf5037685169.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed050737a4369c4e4d1db9417a17244",
+    "url": "/hcp-app/index.html"
+  },
+  {
+    "revision": "f34e608038f626b15baf",
+    "url": "/hcp-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8ee09c0225570c2d1ab0",
+    "url": "/hcp-app/static/css/main.88809522.chunk.css"
+  },
+  {
+    "revision": "d21b747a0d8e9e5eb904",
+    "url": "/hcp-app/static/js/10.074689d3.chunk.js"
+  },
+  {
+    "revision": "b4b0e72201bffa2a62fc",
+    "url": "/hcp-app/static/js/11.65b20625.chunk.js"
+  },
+  {
+    "revision": "b188d757336e7c0d9da5",
+    "url": "/hcp-app/static/js/12.1bc19971.chunk.js"
+  },
+  {
+    "revision": "17ed8a278f3946240f5b",
+    "url": "/hcp-app/static/js/13.b5fbe354.chunk.js"
+  },
+  {
+    "revision": "ef9df876cb09dde27eb4",
+    "url": "/hcp-app/static/js/14.4f3340bd.chunk.js"
+  },
+  {
+    "revision": "f34e608038f626b15baf",
+    "url": "/hcp-app/static/js/2.8f023ddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hcp-app/static/js/2.8f023ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c79c8e5687cdf2bba4",
+    "url": "/hcp-app/static/js/3.fb9b5640.chunk.js"
+  },
+  {
+    "revision": "4e46dfaa7f8f55fc13b1",
+    "url": "/hcp-app/static/js/4.b7dfd95e.chunk.js"
+  },
+  {
+    "revision": "bf2d7b75bce3a4df78fb",
+    "url": "/hcp-app/static/js/5.553a171c.chunk.js"
+  },
+  {
+    "revision": "7f225c2d222311b87f6f",
+    "url": "/hcp-app/static/js/6.938d20f3.chunk.js"
+  },
+  {
+    "revision": "0afbfb425bc7180341f2",
+    "url": "/hcp-app/static/js/7.9cb2f97d.chunk.js"
+  },
+  {
+    "revision": "a4bd0e7ff5c9685c53c3",
+    "url": "/hcp-app/static/js/8.c03006ca.chunk.js"
+  },
+  {
+    "revision": "550ff252be65844bda50",
+    "url": "/hcp-app/static/js/9.2dee65b7.chunk.js"
+  },
+  {
+    "revision": "8ee09c0225570c2d1ab0",
+    "url": "/hcp-app/static/js/main.dcd52a1a.chunk.js"
+  },
+  {
+    "revision": "7c452164feb46336217e",
+    "url": "/hcp-app/static/js/runtime-main.9650195a.js"
+  },
+  {
+    "revision": "e9e6072ed77625132dabe55909754d49",
+    "url": "/hcp-app/static/media/goal.e9e6072e.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/hcp-app/static/media/instagram-sketched.5c570427.png"
+  }
+]);
